4a0cbd8c029b2391156d6a74eadf70a01419f839
nick sachau <nicksachau156@gmail.com>
2023-01-16 01:07:47 -0700

    end of night

--- js/script.js
+++ js/script.js
@@ -1,22 +1,18 @@
 $(function () {
-  const getBirthday = () => {
-    let date = new Date();
-    let birthday = new Date(2004, 02, 03)
+  let date = new Date();
+  let birthday = new Date(2004, 02, 03)
 
-    let age = 0;
+  let age = 0;
 
-    if(date.getMonth() >= birthday.getMonth() && date.getDate() >= birthday.getDate()) {
-      age ++;
-      age = date.getFullYear() - birthday.getFullYear() ;
-    }else {
-      age = date.getFullYear() - birthday.getFullYear() - 1;
-    }
-
-    $('#age').text(age)
-    $('#age2').text(age)
+  if(date.getMonth() >= birthday.getMonth() && date.getDate() >= birthday.getDate()) {
+    age ++;
+    age = date.getFullYear() - birthday.getFullYear() ;
+  }else {
+    age = date.getFullYear() - birthday.getFullYear() - 1;
   }
 
-  getBirthday();
+  $('#age').text(age)
+  $('#age2').text(age)
 
   // Create typing functionality
 })
